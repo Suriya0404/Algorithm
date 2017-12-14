e18835aec9c415a97c2b472a7fada8646145fa0c
Suriya Mohan <mohan.suriya@gene.com>
2017-12-13 16:06:55 -0800

    search algorithm added

--- linkl/coderpad.py
+++ linkl/coderpad.py
@@ -1,9 +1,8 @@
-# from nose.tools import  assert_equal
-
+# from nose.tools import assert_equal
 
 class SingleLinkedList:
-    def __init__(self, val):
-        self.value = val
+    def __init__(self, value):
+        self.value = value
         self.next = None
 
 
@@ -28,7 +27,7 @@ def cyclic_check(node):
 
     return False
 
-# My Solution -
+
 def reverse_singleLinkList(first_node):
     print('Reversing the linked list .... ')
     nodes = []
@@ -64,44 +63,37 @@ def reverse_singleLinkList(first_node):
 
 def jose_reverse_ll(head):
     current_node = head
-    # next_node = None
+    next_node = None
     previous_node = None
 
-    while current_node:
+    while current:
         next_node = current_node.next
         current_node.next = previous_node
-        previous_node = current_node
+        previous_node = current
         current_node = next_node
 
     return previous_node
 
+
 def nth_node_from_last(nth, tail_node):
-
     current_node = tail_node
-    for curr in xrange(nth):
-        current_node =
 
+    for _ in xrange(nth):
+        print(current_node.value)
+        next_node = current_node.next
+        current_node = next_node
+
+    return current_node.value
+
+
+def say_hello():
+    print 'Hello, World'
 
 
 if __name__ == '__main__':
-    # Single Linked List
-    a = SingleLinkedList(10)
-    b = SingleLinkedList(20)
-    c = SingleLinkedList(30)
-    a.next = b
-    b.next = c
+    # for i in xrange(5):
+    #     say_hello()
 
-    # Double Linked List
-    x = DoubleLinkedList(100)
-    y = DoubleLinkedList(200)
-    z = DoubleLinkedList(300)
-
-    x.next = y
-    y.previous = x
-    y.next = z
-    z.previous = y
-
-    # Cyclic check.
     sl1 = SingleLinkedList(10)
     sl2 = SingleLinkedList(20)
     sl3 = SingleLinkedList(30)
@@ -109,13 +101,17 @@ if __name__ == '__main__':
 
     sl1.next = sl2
     sl2.next = sl3
-    # sl3.next = sl1
+    sl3.next = sl4
 
     print(cyclic_check(sl4))
 
+#     node = reverse_singleLinkList(sl1)
 
+#     print(node.value)
+    # print(node.next.value)
+    # print(node.next.value)
 
-
-
+    print('Nth node from last')
+    print(nth_node_from_last(2, sl4))
 
 
--- list/stack_queue_deque.py
+++ list/stack_queue_deque.py
@@ -1,2 +1,84 @@
-class stack:
+class Stack:
     def __init__(self):
+        self.lst = []
+
+    def pop(self):
+        return self.lst.pop()
+
+    def append(self, item):
+        self.lst.append(item)
+
+    def peek(self):
+        return self.lst[len(self.lst) - 1]
+
+
+class Queue:
+    def __init__(self):
+        self.lst = []
+
+    def enqueue(self, item):
+        self.lst.insert(0, item)
+
+    def dequeue(self):
+        self.lst.pop()
+
+    def peek(self):
+        return self.lst[len(self.lst) - 1]
+
+
+# sequential search
+def sequential_search(lst, ele):
+    found = False
+    curr = 0
+
+    if len(lst) == 0:
+        return False
+
+    while curr < len(lst) and not found:
+        if lst[curr] == ele:
+            found = True
+            return found
+
+        curr += 1
+
+    return found
+
+
+def sorted_seq_search(lst, ele):
+    lst_sort = sorted(lst)
+
+    for curr in xrange(len(lst_sort)):
+        if lst_sort[curr] == ele:
+            return True
+        elif lst_sort[curr] > ele:
+            return False
+
+    return False
+
+
+def binary_search(lst, ele):
+    lst_sorted = sorted(lst)
+    print(lst_sorted)
+
+    mid = len(lst_sorted) / 2
+
+    print('Middle element: ' + str(lst_sorted[mid]))
+
+    if lst_sorted[mid] == ele:
+        return True
+    elif len(lst_sorted) == 1:
+        return False
+    elif lst_sorted[mid] > ele:
+        return binary_search(lst_sorted[:mid], ele)
+    elif lst_sorted[mid] < ele:
+        return binary_search(lst_sorted[mid:], ele)
+
+
+if __name__ == '__main__':
+    lst = [23, 45, 56, 12, 78, 87, 95]
+
+    print(sequential_search(lst, 78))
+
+    print(sorted_seq_search(lst, 23))
+
+    print(binary_search(lst, 49))
